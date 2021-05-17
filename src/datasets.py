fix Average Precision

src/datasets.py
@@ -204,6 +204,8 @@ class MultiObjectDataset(Dataset):
         except:
             labels = data['labels']
             print(type(labels))
+            
+
 
         # Split train and test
         split = int(split * len(x))
@@ -216,12 +218,13 @@ class MultiObjectDataset(Dataset):
         # they might have different sizes)
         self.x = torch.from_numpy(x[indices])
         
+        """
         try:
             labels.pop('text', None)
             labels.pop('brut', None)
         except:
             print("No text to pop !")
-        
+        """
         self.labels = self._labels_to_tensorlist(labels, indices)
 
 

src/model.py
@@ -227,7 +227,7 @@ class Monet(nn.Module):
         
         
 class MonetClassifier(nn.Module):
-    def __init__(self, conf, height, width, dim_points, dim_rela = 0):
+    def __init__(self, conf, height, width, dim_points):
         super().__init__()
         self.conf = conf
         self.attention = AttentionNet(conf)
@@ -240,18 +240,6 @@ class MonetClassifier(nn.Module):
                                  nn.ReLU(),
                                  nn.Linear(64, dim_points),
                                  nn.Sigmoid())
-                                       
-        #Flag for relationship prediction
-        if dim_rela > 0:
-            self.pred_rela = True
-            self.dim_rela = dim_rela
-        else:
-            self.pred_rela = False
-            
-        if self.pred_rela:
-            self.MLP_binary_rela = nn.Sequential(nn.Linear(2*self.dim_z, 64),
-                                                 nn.ReLU(),
-                                                 nn.Linear(64, dim_rela))
         
         self.beta = 0.5
         self.gamma = 0.25
@@ -265,20 +253,27 @@ class MonetClassifier(nn.Module):
         masks.append(scope)
         
         outputs = []
+        
+        latent_vectors = []
 
         for i, mask in enumerate(masks):
             z = self.__encoder_step(x, mask)
+            latent_vectors.append(z)
             outputs.append(self.MLP(z))
             
         outputs = torch.stack(outputs, dim = 1)
+        latent_vectors = torch.stack(latent_vectors, dim = 1)
         masks = torch.cat(masks, 1)
+        
+        dict = {'outputs_slot': outputs, 'masks': masks, 'latent_vectors': latent_vectors}
   
-        return outputs, masks
+        return dict
         
     def get_loss(self, x, target, loss_function):
-        outputs, _ = self.forward(x)
+        dict = self.forward(x)
+        outputs = dict['outputs_slot']
         loss = loss_function(outputs, target)
-        return outputs, loss
+        return dict, loss
 
 
     def __encoder_step(self, x, mask):
@@ -404,7 +399,7 @@ class SoftPositionEmbed(nn.Module):
 
 
 class SlotAttentionClassifier(nn.Module):
-    def __init__(self, conf, height, width, dim_points, dim_rela = 0):
+    def __init__(self, conf, height, width, dim_points):
         super().__init__()
         self.conf = conf
 
@@ -453,10 +448,124 @@ class SlotAttentionClassifier(nn.Module):
         # x.shape [batch_size, num_slots, slot_size]
 
         predictions = self.mlp_classifier(slots)
+        
+        dict = {'outputs_slot': predictions, 'masks': None, 'latent_vectors': slots}
 
-        return predictions, None
+        return dict
         
     def get_loss(self, x, target, loss_function):
-        outputs, _ = self.forward(x)
+        dict = self.forward(x)
+        outputs = dict['outputs_slot']
         loss = loss_function(outputs, target)
-        return outputs, loss
+        return dict, loss
+        
+        
+##############################################################################################################
+##                                                                                                          ##
+##                                         RELATIONS PREDICTOR                                              ##
+##                                                                                                          ##
+##############################################################################################################       
+
+
+class RelationsPredictor(nn.Module):
+    def __init__(self, conf, height, width, dim_points, dim_rela, object_classifier = 'slot_att'):
+        
+        self.conf = conf
+        
+        self.alpha = 1.
+        
+        if object_classifier == 'monet':
+            self.obj_class = MonetClassifier(conf, height, width, dim_points)
+        elif object_classifier == 'slot_att':
+            self.obj_class = SlotAttentionClassifier(conf, height, width, dim_points)
+            
+        
+        self.mlp_rela = nn.Sequential(nn.Linear(2*64, 64),
+                                            nn.ReLU(),
+                                            nn.Linear(64, dim_rela),
+                                            nn.Sigmoid())    
+            
+        
+            
+    def forward(self, images):
+        
+        dict = self.obj_class(images)
+        
+        latent_vectors = dict['latent_vectors']
+        
+        _, num_slots, _ = latent_vectors.shape
+        
+        outputs_rela = []
+        index_to_pair = []
+        
+        for k in range(num_slots):
+            for l in range(k):
+                z_k, z_l = latent_vectors[:, k, :], latent_vectors[:, l, :]
+                z_kl = torch.cat((z_k, z_l), dim = -1)
+                z_lk = torch.cat((z_l, z_k), dim = -1)
+                
+                rela_kl = self.mlp_rela(z_kl)
+                rela_lk = self.mlp_rela(z_lk)
+                
+                outputs_rela.append(rela_kl)
+                outputs_rela.append(rela_lk)
+                
+                index_to_pair.append([k, l])
+                index_to_pair.append([l, k])
+        
+        outputs_rela = torch.stack(outputs_rela, dim = 1)
+        
+        dict['outputs_rela'] = outputs_rela
+        dict['index_to_pair'] = index_to_pair
+        
+        return dict
+        
+        
+    def get_loss(self, x, target, loss_function):
+        
+        dict = self.forward(x)
+        
+        outputs_slot = dict['outputs_slot']
+        outputs_rela = dict['outputs_rela']
+        index_to_pair = dict['index_to_pair']
+        
+        loss_obj_class = loss_function(outputs_slot, target)
+        
+        loss_rela_class = torch.zeros(1).cuda()
+        
+        loss = loss_obj_class + self.alpha * loss_rela_class
+        
+        return dict, loss
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

src/train.py
@@ -50,7 +50,7 @@ def unsupervised_experiment(args, vis):
     
     
 def supervised_experiment(args, vis):
-    
+    print("Reading Config File ...")
     #Get config file
     with open(args.config) as config_file:
         config = yaml.safe_load(config_file)['datasets'][args.dataset]
@@ -64,15 +64,16 @@ def supervised_experiment(args, vis):
         DataLoader = torch.utils.data.DataLoader
     else:
         DataLoader = utils.import_from_path(config['dataloader']['filepath'], config['dataloader']['class'])
-
+    
+    print("Loading Data ...")
     #Create DataLoader
     trainset = ObjectDataset(config['prediction'][args.prediction]['filepath'], train=True, transform= transform)
     
     valset = ObjectDataset(config['prediction'][args.prediction]['filepath'], train=False, transform= transform)
     
-    trainloader = DataLoader(trainset, batch_size=config['params']['batch_size'], shuffle=True, num_workers=0)
+    trainloader = DataLoader(trainset, batch_size=config['params']['batch_size'], shuffle=True, num_workers=2)
     
-    valoader = DataLoader(trainset, batch_size=config['params']['batch_size'], shuffle=False, num_workers=0)
+    valoader = DataLoader(trainset, batch_size=config['params']['batch_size'], shuffle=False, num_workers=2)
     
     #Create Model
     try:
@@ -85,13 +86,11 @@ def supervised_experiment(args, vis):
     if args.model == 'monet':
         model_net = model.MonetClassifier(config['params'], config['params']['height'],
                                                         config['params']['width'],
-                                                        config['prediction'][args.prediction]['dim_points'],
-                                                        dim_rela).cuda()
+                                                        config['prediction'][args.prediction]['dim_points']).cuda()
     elif args.model == 'slot_att':
         model_net = model.SlotAttentionClassifier(config['params'], config['params']['height'],
                                                         config['params']['width'],
-                                                        config['prediction'][args.prediction]['dim_points'],
-                                                        dim_rela).cuda()
+                                                        config['prediction'][args.prediction]['dim_points']).cuda()
     else:
         raise Exception("Error in the model name. Make sure it is in {monet, slot_att}.")
     
@@ -190,7 +189,6 @@ def run_training_supervised(model, conf, dataset, pred, trainloader, valoader, v
  
         return factor
 
-    #optimizer = optim.RMSprop(monet.parameters(), lr=1e-4)
     optimizer = optim.Adam(model.parameters(), lr = base_learning_rate,  eps=1e-08)
     scheduler = optim.lr_scheduler.LambdaLR(optimizer, learning_rate_scheduler)
     criterion = utils.hungarian_huber_loss
@@ -202,7 +200,7 @@ def run_training_supervised(model, conf, dataset, pred, trainloader, valoader, v
             images, labels = images.cuda(), get_ground_truth(labels, conf, pred).cuda()
 
             optimizer.zero_grad()
-            output, loss = model.get_loss(images, labels, criterion)
+            dict, loss = model.get_loss(images, labels, criterion)
             loss.backward()
             optimizer.step()
 
@@ -215,20 +213,28 @@ def run_training_supervised(model, conf, dataset, pred, trainloader, valoader, v
 
                     print('[%d, %5d] loss: %.3f' %
                           (epoch + 1, global_step, running_loss))
-                
-                    vis.plotline('loss', 'train', 'Loss', global_step, running_loss)
-                    vis.plotline('lr', 'train', 'Learning Rate', global_step, optimizer.param_groups[0]['lr'])
-
-                    #running_loss = 0.0
+                    
+                    try:
+                        vis.plotline('loss', 'train', 'Loss', global_step, running_loss)
+                        vis.plotline('lr', 'train', 'Learning Rate', global_step, optimizer.param_groups[0]['lr'])
+                    except:
+                        pass
 
                     if global_step % 500 == 0:
-
+                        
+                        output = dict['outputs_slot']
                         color_precision, shape_precision, size_precision = training_monitor.get_carac_precision(output.detach(), labels.detach())
-
-                        vis.plotline('carac_precision', 'shape', 'Carac Precision', global_step, shape_precision)
-                        vis.plotline('carac_precision', 'size', 'Carac Precision', global_step, size_precision)
-                        vis.plotline('carac_precision', 'color', 'Carac Precision', global_step, color_precision)
-
+                        
+                        try:
+                            vis.plotline('carac_precision', 'shape', 'Carac Precision', global_step, shape_precision)
+                            vis.plotline('carac_precision', 'size', 'Carac Precision', global_step, size_precision)
+                            vis.plotline('carac_precision', 'color', 'Carac Precision', global_step, color_precision)
+                        except:
+                            pass
+                        
+                        print('Carac Precision : shape %.3f ; size %.3f ; color %.3f' % (shape_precision, size_precision, color_precision))
+    
+                        
                         #vis.plotimage('image1', utils.numpify(images[0]), output_to_title(output[0]))
                         #vis.plotimage('image2', utils.numpify(images[1]), output_to_title(output[1]))
                         #vis.plotimage('image3', utils.numpify(images[2]), output_to_title(output[2]))
@@ -236,17 +242,20 @@ def run_training_supervised(model, conf, dataset, pred, trainloader, valoader, v
 
                         model.eval()
 
-                        #ap_train = [utils.average_precision(output.detach().cpu().numpy(), labels.detach().cpu().numpy(), d, dataset) for d in [-1., 1., 0.5, 0.25, 0.125] ]
-
                         images, labels = iter(valoader).next()
                         labels = get_ground_truth(labels, conf, pred)
-                        output, _ = model(images.cuda())
+                        dict = model(images.cuda())
+                        output = dict['outputs_slot']
                         ap = [utils.average_precision(output.detach().cpu().numpy(), labels.detach().numpy(), d, dataset) for d in [-1., 1., 0.5, 0.25, 0.125] ]
                 
-                        vis.plotline('AP', 'inf', 'Average Precision', global_step, ap[0] )
-                        vis.plotline('AP', '1', 'Average Precision', global_step, ap[1] )
+                        try:
+                            vis.plotline('AP', 'inf', 'Average Precision', global_step, ap[0] )
+                            vis.plotline('AP', '1', 'Average Precision', global_step, ap[1] )
+                        except:
+                            pass
                 
-                    
+                        print('AP : inf %.3f ; 1. %.3f ; 0.5 %.3f' % (ap[0], ap[1], ap[2]))
+                        
                         model.train() 
             
             global_step += 1